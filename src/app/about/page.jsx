9941c78acd6d11a9e22c947e867639dde6e6c380
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-05-18 23:18:45 +0500

    create about page.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,34 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa magnam
+        asperiores esse. Suscipit placeat qui, laudantium animi itaque
+        reprehenderit quo dicta magnam quasi quod officiis porro ipsam nulla.
+      </p>
+      <p>
+        Optio quae ut delectus autem cupiditate natus fugit eveniet possimus.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa magnam
+        asperiores esse. Suscipit placeat qui, laudantium animi itaque
+        reprehenderit quo dicta magnam quasi quod officiis porro ipsam nulla
+        expedita blanditiis maxime illo quis dolorum deserunt facere, tenetur,
+        enim eaque voluptates. Optio quae ut delectus autem cupiditate natus
+        fugit eveniet possimus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa magnam
+        asperiores esse. Suscipit placeat qui, laudantium animi itaque
+        reprehenderit quo dicta magnam quasi quod officiis porro ipsam nulla
+        expedita blanditiis maxime illo quis dolorum deserunt facere, tenetur,
+        enim eaque voluptates. Optio quae ut delectus autem cupiditate natus
+        fugit eveniet possimus. Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Culpa magnam asperiores esse. Suscipit placeat qui,
+        laudantium animi itaque reprehenderit quo dicta magnam quasi quod
+        officiis porro ipsam nulla expedita blanditiis maxime illo quis dolorum
+        deserunt facere, tenetur, enim eaque voluptates. Optio quae ut delectus
+        autem cupiditate natus fugit eveniet possimus.
+      </p>
+    </div>
+  );
+}
