c625b5b271c9b775c658c12eb33b5b41225fbe75
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-05-18 19:26:45 +0500

    add dark mode functionality in header compoent.

---
+++ src/app/Providers.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+
+export default function Providers({ children }) {
+  return (
+    <ThemeProvider defaultTheme="system" attribute="class">
+      <div className="to-gray-700 dark:to-gray-200 dark:bg-gray-700 min-h-screen select-none transition-colors duration-300">
+        {children}
+      </div>
+    </ThemeProvider>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import DarkModeSwitch from "./DarkModeSwitch";
 export default function Header() {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
@@ -9,12 +10,15 @@ export default function Header() {
         <MenuItem title="home" address="/" Icon={AiFillHome} />
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href={"/"} className="flex gap-1 items-center">
-        <span className="text-2xl font-bold py-2 px-1 rounded-lg bg-amber-500">
-          IMDb
-        </span>
-        <span className="text-xl hidden sm:inline">Clone</span>
-      </Link>
+      <div className="flex gap-4 justify-center items-center">
+        <DarkModeSwitch />
+        <Link href={"/"} className="flex gap-1 items-center ">
+          <span className="text-2xl font-bold py-2 px-1 rounded-lg bg-amber-500">
+            IMDb
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
+        </Link>
+      </div>
     </div>
   );
 }
